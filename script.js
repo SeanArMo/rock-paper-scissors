3da1f8972debaa00bc813b8b98c0374a951f202c
SeanArMo <107956039+SeanArMo@users.noreply.github.com>
2022-10-05 19:10:53 +0000

    JS ui Updated

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const choices = document.querySelectorAll('.choice')
+const results = document.querySelector('.results')
+
+const result = document.createElement('p');
+const scoreboard = document.createElement('p');
+
 let playerScore = 0;
 let computerScore = 0;
+
+let gameRun = playerScore < 5 && computerScore < 5;
+
+result.textContent = 'No results yet, click one of the choices to start the game!'
+results.appendChild(result);
+
+scoreboard.textContent = `${playerScore} | ${computerScore}`;
+results.appendChild(scoreboard);
 
 function getComputerChoice () {
     const choices = ["rock", "paper", "scissors"];
@@ -8,53 +22,66 @@ function getComputerChoice () {
     return computerChoice;
 }
 
-function getUserChoice() {
-    let userChoice = prompt("What is your choice (rock/paper/scissors): ");
-    userChoice = userChoice.toLowerCase();
-    return userChoice;
+function checkWinner(playerScore, computerScore) {
+    if (playerScore == 5) {
+        result.textContent = `You Won with ${playerScore} points! Computer had ${computerScore} points!`;
+        results.appendChild(result);
+    } else {
+        result.textContent = `You Lost with ${playerScore} points. Computer had ${computerScore} points.`;
+        results.appendChild(result);
+    }
 }
 
 function playRound (userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        console.log("This round's a tie, you both chose " + userChoice + "!");
-    } else if (userChoice === "rock" && computerChoice === "paper"){
-        console.log("You Lost this round. You chose " + userChoice + " and computer chose " + computerChoice +".");
-        computerScore++;
-    }else if (userChoice === "rock" && computerChoice === "scissors"){
-        console.log("You won this round! You chose " + userChoice + " and computer chose " + computerChoice +"!");
-        playerScore++;
-    }else if (userChoice === "paper" && computerChoice === "scissors"){
-        console.log("You Lost this round. You chose " + userChoice + " and computer chose " + computerChoice +".");
-        computerScore++;
-    }else if (userChoice === "paper" && computerChoice === "rock"){
-        console.log("You won this round! You chose " + userChoice + " and computer chose " + computerChoice +"!");
-        playerScore++;
-    }else if (userChoice === "scissors" && computerChoice === "rock"){
-        console.log("You Lost this round. You chose " + userChoice + " and computer chose " + computerChoice +".");
-        computerScore++;
-    }else if (userChoice === "scissors" && computerChoice === "paper"){
-        console.log("You won this round! You chose " + userChoice + " and computer chose " + computerChoice +"!");
-        playerScore++;
-    } else {
-        console.log("Invalid input :(")
-    }
-}
-
-function checkWinner (playerScore, computerScore) {
-    if (playerScore === computerScore) {
-        console.log("It's a draw, you both got " + playerScore + ".");
-    } else if (playerScore > computerScore) {
-        console.log("You won! You have " + playerScore + " point(s) and computer has " + computerScore + ".");
-    } else if (playerScore < computerScore) {
-        console.log("You lost! You have " + playerScore + " point(s) and computer has " + computerScore + ".");
-    } else {
-        console.log("Error");
-    }
-}
-
-for (let i = 0; i < 5; i++) {
-    playRound(getUserChoice(), getComputerChoice());
-    if (i === 4) {
+    if (gameRun){
+        if (userChoice === computerChoice) {
+            result.textContent = `This round's a tie, you both chose ${userChoice}!`;
+            results.appendChild(result);
+        } else if (userChoice === "rock" && computerChoice === "paper"){
+            result.textContent = `You Lost this round. You chose ${userChoice} and computer chose ${computerChoice}.`;
+            results.appendChild(result);
+            computerScore++;
+        }else if (userChoice === "rock" && computerChoice === "scissors"){
+            result.textContent = `You won this round! You chose ${userChoice} and computer chose ${computerChoice}!`;
+            results.appendChild(result);
+            playerScore++;
+        }else if (userChoice === "paper" && computerChoice === "scissors"){
+            result.textContent = `You Lost this round. You chose ${userChoice} and computer chose ${computerChoice}.`;
+            results.appendChild(result);
+            computerScore++;
+        }else if (userChoice === "paper" && computerChoice === "rock"){
+            result.textContent = `You won this round! You chose ${userChoice} and computer chose ${computerChoice}!`;
+            results.appendChild(result);
+            playerScore++;
+        }else if (userChoice === "scissors" && computerChoice === "rock"){
+            result.textContent = `You Lost this round. You chose ${userChoice} and computer chose ${computerChoice}.`;
+            results.appendChild(result);
+            computerScore++;
+        }else if (userChoice === "scissors" && computerChoice === "paper"){
+            result.textContent = `You won this round! You chose ${userChoice} and computer chose ${computerChoice}!`;
+            results.appendChild(result);
+            playerScore++;
+        } else {
+            result.textContent = "Invalid input :(";
+            results.appendChild(result);
+        }
+    }else {
         checkWinner(playerScore, computerScore);
     }
- }
+}
+
+function removeTransition (e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('animation');
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', (e) => {
+        choice.classList.add('animation');
+        playRound(choice.id, getComputerChoice());
+        gameRun = playerScore < 5 && computerScore < 5;
+        scoreboard.textContent = `${playerScore} | ${computerScore}`;
+        results.appendChild(scoreboard);
+        choices.forEach(choice => choice.addEventListener('transitionend', removeTransition));
+    })
+});
